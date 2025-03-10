e3937c52cad853f52f11408e9ac5a45238669f82
caiojorge <caio.jorge@gmail.com>
2025-03-10 16:45:39 -0300

    Revert "criando o s3 para compartilhar a vpc com o eks"
    This reverts commit c587cc21b58bccb2a0dcbae85702f09770876ee9.

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-states"    # nome do seu bucket
-    key    = "rds/terraform.tfstate"  # caminho onde o state do RDS será salvo
-    region = "us-east-1"             # mesma região do bucket S3
-  }
-}
--- outputs.tf
+++ outputs.tf
@@ -12,26 +12,3 @@ output "db_name" {
   description = "Nome do banco"
   value       = aws_db_instance.default.db_name
 }
-
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id
-}
-
-output "private_subnets" {
-  description = "Lista de subnets privadas"
-  value       = [
-    aws_subnet.private_subnet_a.id,
-    aws_subnet.private_subnet_b.id
-  ]
-}
-
-output "public_subnet" {
-  description = "Subnet pública para NAT"
-  value       = aws_subnet.public_subnet.id
-}
-
-output "rds_security_group_id" {
-  description = "Security Group do RDS"
-  value       = aws_security_group.rds_sg.id
-}
